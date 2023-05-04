7dff7c28a5cb56105bf0f3f7c7536065649bf7df
isuri97 <you@example.com>
2023-05-04 19:30:07 +0530

    amend with the datafile

--- model/coref.py
+++ model/coref.py
@@ -4,18 +4,22 @@ import spacy
 
 import crosslingual_coreference
 
-text = (
-    "Do not forget about Momofuku Ando! He created instant noodles in Osaka. At"
-    " that location, Nissin was founded. Many students survived by eating these"
-    " noodles, but they don't even know him.Noodels were very tasty and it is very popular ammong the Japan"
-)
+df = pd.read_csv('data/new_wiener.csv',',')
+content = df['content']
+print(content)
 
-
+# text = (
+#     "Do not forget about Momofuku Ando! He created instant noodles in Osaka. At"
+#     " that location, Nissin was founded. Many students survived by eating these"
+#     " noodles, but they don't even know him.Noodels were very tasty and it is very popular ammong the Japan"
+# )
+#
+#
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe(
     "xx_coref", config={"chunk_size": 2500, "chunk_overlap": 2, "device": -1}
 )
 
-doc = nlp(text)
+doc = nlp(content[0])
 print(doc._.coref_clusters)
 print(doc)
