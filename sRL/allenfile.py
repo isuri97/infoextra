e8dcc6bff4d20f6f841062f8d279aa4ad4eeea4e
isuri97 <you@example.com>
2023-05-21 10:37:01 +0530

    commit srl folder

--- sRL/allenfile.py
+++ sRL/allenfile.py
@@ -13,32 +13,30 @@ df['coref']
 count = 0
 
 for index, row in df.iterrows():
-  paragraph = row['coref']
-  try:
-    sentences = nltk.sent_tokenize(paragraph)
-    print(sentences)
-    filename = f"sRL/sentence_files/{index}.txt"
-    with open(filename, 'w') as f:
-      for sentence in sentences:
-          parts = re.split(r"\s+and\s+", sentence)
-          if parts is not None:
-            for i in parts:
-              f.write(i + '\n')
-          else:
-            f.write(sentence + '\n')
-    count = count + 1
-  except TypeError:
-    pass
-
+    paragraph = row['coref']
+    try:
+        sentences = nltk.sent_tokenize(paragraph)
+        print(sentences)
+        filename = f"sRL/sentence_files/{index}.txt"
+        with open(filename, 'w') as f:
+            for sentence in sentences:
+                parts = re.split(r"\s+and\s+", sentence)
+                if parts is not None:
+                    for i in parts:
+                        f.write(i + '\n')
+                else:
+                    f.write(sentence + '\n')
+        count = count + 1
+    except TypeError:
+        pass
 
 from pathlib import Path
-import os,glob
+import os, glob
 
 folder = Path('sRL/sentence_files')
 
 # get all the files in the folder
-files = folder.glob('**/*.txt') # assuming the files are csv
-
+files = folder.glob('**/*.txt')  # assuming the files are csv
 
 for filename in glob.glob(os.path.join(folder, '*.txt')):
     print(filename)
@@ -61,7 +59,8 @@ for filename in glob.glob(os.path.join(folder, '*.txt')):
         except IndexError:
             pass
 
-        fn = f"sRL/files_dir/{count}.txt"
+        fn = "sRL/files_dir/" + count + ".txt"
+
         with open(fn, 'w') as ff:
             # print(f'sentence list = {sentences_list}')
             ff.writelines(sentences_list)
